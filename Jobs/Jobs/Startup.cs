9ccd2c33dc109bc1ee091c5f166b133824ae3aea
ArnulfoAguilar <arnulfo.aguilar9405@gmail.com>
2021-01-06 09:11:42 -0600

    change the initial configuration to Jobs index for default

--- Jobs/Jobs/Startup.cs
+++ Jobs/Jobs/Startup.cs
@@ -63,7 +63,7 @@ namespace Jobs
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Job}/{action=Index}/{id?}");
             });
         }
         
