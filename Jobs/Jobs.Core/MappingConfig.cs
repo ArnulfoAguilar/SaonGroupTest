f0c49dc530f8bdc44b2fc3c1c3c10d94200c20d0
ArnulfoAguilar <arnulfo.aguilar9405@gmail.com>
2021-01-05 20:22:32 -0600

    Adding and configure a AutoMapper nuguet for mapping the entities

---
+++ Jobs/Jobs.Core/MappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jobs.Core
+{
+    public class MappingConfig
+    {
+        public static void Initialize()
+        {
+            Mapper.Initialize(config => config.AddProfile(new MappingProfile()));
+        }
+    }
+}
---
+++ Jobs/Jobs.Core/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jobs.Core
+{
+    class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Domain -> Dto
+            //CreateMap<Domain.Responses.ResponseStatus, Dtos.ResponseStatus>();
+        }
+    }
+}
